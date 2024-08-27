bd25c6d35ba7c85ddaeffc46582f96a095568c76
Vincent van Hees <v.vanhees@accelting.com>
2024-08-27 20:05:51 +0200

    update script to test with circame data

--- inst/test_code.R
+++ inst/test_code.R
@@ -3,44 +3,50 @@ graphics.off()
 # Install: https://miktex.org/download
 # On Windows: install tabu package: https://stackoverflow.com/questions/47613096/tabu-sty-not-found
 library(patientGGIR)
-
-GGIRoutputdir = "D:/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/output_pilot2022"
+library(kableExtra)
+source("R/createReport.R")
+source("R/plot_lux_sleep_act_cr.R")
+source("R/prepareTable.R")
+source("R/introText.R")
+source("R/writeReportTable.R")
+# GGIRoutputdir = "D:/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/output_pilot2022"
+# idsep = "_"
+GGIRoutputdir = "D:/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/Circame_2024_bin/output_S20_2024_copie"
+idsep = "[.]"
 lang = "fr"
-# # Run plot code in isolation
+
+# # # Run plot code in isolation
 # source("R/plot_lux_sleep_act_cr.R")
 # getID = function(x) {
 #   x = gsub(pattern = "meta_", replacement = "", x = x)
-#   return(unlist(strsplit(x, "_"))[1])
+#   return(unlist(strsplit(x, idsep))[1])
 # }
 # ids = unlist(lapply(dir(paste0(GGIRoutputdir, "/meta/basic"), full.names = FALSE), FUN = getID))
 # 
 # 
 # x11()
-# plot_lux_sleep_act_cr(GGIRoutputdir = GGIRoutputdir, id = ids[1],
+# plot_lux_sleep_act_cr(GGIRoutputdir = GGIRoutputdir, id = ids[6],
 #                       lang = lang, desiredtz = "Europe/London")
+# kkkk
+
 # 
 # 
-
-
+# 
 # # Run prepare table code in isolation
 # source("R/prepareTable.R")
 # getID = function(x) {
 #   x = gsub(pattern = "meta_", replacement = "", x = x)
-#   return(unlist(strsplit(x, "_"))[1])
+#   return(unlist(strsplit(x, idsep))[1])
 # }
 # ids = unlist(lapply(dir(paste0(GGIRoutputdir, "/meta/basic"), full.names = FALSE), FUN = getID))
 # 
-# data = prepareTable(GGIRoutputdir, id = ids[1], lang)
-
-
+# data = prepareTable(GGIRoutputdir, id = ids[6], lang)
+# 
+# 
 # kkkk
 # Run code as called from within Markdown
 creatReport(GGIRoutputdir = GGIRoutputdir,
-            lang = lang, idsep = "_", desiredtz = "Europe/London", type = "onepage_luxsleepactcr_A4",
+            lang = lang, idsep = idsep, desiredtz = "Europe/Paris", type = "onepage_luxsleepactcr_A4",
             deviceName = "GENEActiv")
 
-
-
-# col = rainbow(n = 200)
-# plot(1:200, rep(1, 200), col = col, pch = 20, type = "p", cex= 10)
 
