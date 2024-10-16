3cb729751b7d3949bb28173088995858b7f6b8b3
Vincent van Hees <vincentvanhees@gmail.com>
2024-10-16 11:40:56 +0200

    latest updates to test_code

--- inst/test_code.R
+++ inst/test_code.R
@@ -12,11 +12,12 @@ library(kableExtra)
 # GGIRoutputdir = "D:/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/output_pilot2022"
 # idsep = "_"
 # GGIRoutputdir = "D:/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/Circame_2024_bin/output_S20_2024_copie"
-GGIRoutputdir = "~/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/Circame_2024_bin/output_S20_2024_copie"
+# GGIRoutputdir = "~/Dropbox/Work/sharedfolder/projects/ERC_Paris/Clinic/Circame_2024_bin/output_S20_2024_copie"
+GGIRoutputdir = "~/data/ERC_Circame/output_bins"
 idsep = "[.]"
 lang = "fr"
 
-# # # Run plot code in isolation
+# # # ================================================
 # source("R/plot_lux_sleep_act_cr.R")
 # getID = function(x) {
 #   x = gsub(pattern = "meta_", replacement = "", x = x)
@@ -28,12 +29,11 @@ lang = "fr"
 # x11()
 # plot_lux_sleep_act_cr(GGIRoutputdir = GGIRoutputdir, id = ids[6],
 #                       lang = lang, desiredtz = "Europe/London")
-# kkkk
 
 # 
 # 
-# 
-# # Run prepare table code in isolation
+# # # ================================================
+# # # Run prepare table code in isolation
 # source("R/prepareTable.R")
 # getID = function(x) {
 #   x = gsub(pattern = "meta_", replacement = "", x = x)
@@ -42,8 +42,8 @@ lang = "fr"
 # ids = unlist(lapply(dir(paste0(GGIRoutputdir, "/meta/basic"), full.names = FALSE), FUN = getID))
 # 
 # data = prepareTable(GGIRoutputdir, id = ids[6], lang)
-# 
-# 
+
+
 # Run code as called from within Markdown
 creatReport(GGIRoutputdir = GGIRoutputdir,
             lang = lang, idsep = idsep, desiredtz = "Europe/Paris", type = "onepage_luxsleepactcr_A4",
